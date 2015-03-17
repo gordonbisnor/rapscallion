be5522e343605e93329114180db0b7655d3d6ad1
Gordon B. Isnor <gordonbisnor@gmail.com>
2015-03-17 15:03:46 -0300

    issues with confirmation fields

--- app/assets/javascripts/rapscallion/validations.js
+++ app/assets/javascripts/rapscallion/validations.js
@@ -8,21 +8,21 @@
 
 */
 
-(function($){
+(function ($) {
 
-  $.fn.rapscallion = function(options){
+  $.fn.rapscallion = function (options) {
 
     var settings = $.extend({
-    
+
       /* error messages will be shown in a div set the class for that here */
       error_message_container_class: "error_messages",
-      
+
       /* fields with errors will get a class to indicate errors, change that class name here */
       field_with_error_class: 'has_error',
-      
+
       /* valid fields will get a class to indicate success, change that class name here */
       field_valid_class: 'is_valid',
-      
+
       /* error messages will be remove from input container, eg closest(:selector), set that here */
       field_container: 'div.input',
 
@@ -31,43 +31,43 @@
 
     }, options);
 
-    return this.each(function() {
-      
-      $(this).on(settings.trigger, function(){
+    return this.each(function () {
 
-        /* the input in question */  
+      $(this).on(settings.trigger, function () {
+
+        /* the input in question */
         var el = $(this);
-        
+
         /* the form */
         var form = el.closest('form');
-        
+
         /* model reference from field name eg user from user[username] */
-        var klass = el.prop('name').replace(/\[.*\]/,'');
-        
+        var klass = el.prop('name').replace(/\[.*\]/, '');
+
         /* field name eg username from user[username] */
-        var attr = el.prop('id').replace(klass+"_",'');
-        
+        var attr = el.prop('id').replace(klass + "_", '');
+
         /* field value */
         var field_val = el.val();
-        
+
         /* set up basic hash for validation */
-        validation_data = {klass: klass, attr: attr, field_val: field_val}
+        validation_data = {klass: klass, attr: attr, field_val: field_val};
 
         /* 
         confirmation fields are special cases, and add the field to confirm for 
         eg password and password_confirmation
         */
-        if(attr.match(/.*_confirmation/) != undefined){
-          var confirmation_field = attr.replace('_confirmation','');
+        if (attr.match(/(.*)_confirmation/) !== undefined) {
+          var confirmation_field = attr.replace('_confirmation', '');
           var confirmation_value = form.find('#' + klass + "_" + confirmation_field).val();
-        };
+        }
 
-        validation_data[confirmation_field] = confirmation_value;
+        validation_data['confirmation_field'] = confirmation_value;
 
         /* existing record? */
-        if(form.data('existing-record') != undefined) {
+        if (form.data('existing-record') !== undefined) {
           validation_data['existing_record'] = form.data('existing-record');
-        };
+        }
 
         /* do ajax request */
         $.ajax({
@@ -76,24 +76,24 @@
           dataType: 'json',
           data: validation_data,
 
-          success: function(data, status, xhr) {
-            
+          success: function (data) {
+
             /* remove existing error messages */
-            el.closest(settings.field_container).find('.'+settings.error_message_container_class).remove();
-        
+            el.closest(settings.field_container).find('.' + settings.error_message_container_class + ', .errors, .error').remove();
+
             /* if no errors present field is valid, add is_valid css class */
-            if(data.length == 0) {
+            if (data.length === 0) {
+              console.log('is valid');
               el.removeClass(settings.field_with_error_class).addClass(settings.field_valid_class);
 
-            /* if error present field is not valid, add has_error class to input, and error messages after input */  
-            
+            /* if error present field is not valid, add has_error class to input, and error messages after input */
             } else {
               /* cycle through json response adding errors for each */
-              $.each( data, function( key, val ) {
+              $.each(data, function (key, val) {
                 el.after('<div class="' + settings.error_message_container_class + '">' + val + '</div>');
-                el.removeClass(settings.field_valid_class).addClass(settings.field_with_error_class)
+                el.removeClass(settings.field_valid_class).addClass(settings.field_with_error_class);
               });
-            }; /* end if errors or not */
+            } /* end if errors or not */
 
           } /* end success */
 
@@ -101,10 +101,10 @@
 
 
       });
-    
+
 
     });
-    
-   
+
+
   };
 }(jQuery));
--- app/controllers/rapscallion/validations_controller.rb
+++ app/controllers/rapscallion/validations_controller.rb
@@ -3,38 +3,30 @@ module Rapscallion
 	class ValidationsController < ::ApplicationController
 
 		eval %[skip_filter #{_process_action_callbacks.select { |c|
-    [:before, :after, :around].include? c.kind }.collect{|c|
-      c.filter.inspect}.join(", ")
-    }]
+	  	[:before, :after, :around].include? c.kind }.collect{|c|
+	    c.filter.inspect}.join(", ")
+	  }]
 
 		def create
 			klass = params[:klass].camelize.constantize
-
-			if params[:existing_record].present?
-				item = klass.find(params[:existing_record])
-			else
-				item = klass.new
-			end
-
+			item = params[:existing_record].present? ? klass.find(params[:existing_record]) : klass.new
 			field = params[:attr]
 			value = params[:field_val]
 			item.send("#{field}=", value)
 			item.valid?
 			errors = item.errors[field].uniq
-
-			if !field.match(/_confirmation/).nil?
-				field_to_confirm_against = field.gsub('_confirmation', '')
-				x = params[field_to_confirm_against.to_sym]
-				
-				errors = ["doesn't match confirmation"]  unless value == x
-			end
-
+			errors = check_confirmation_field_match(field, value) if !field.match(/_confirmation/).nil?
 			render json: errors.to_json
-			
 			rescue => e
 				logger.info "ERROR: #{e}"
+		end # end create
+
+		def check_confirmation_field_match(field, value)
+			field_to_confirm_against = field.gsub('_confirmation', '')
+			value == params['confirmation_field'] ? [] : ["doesn't match confirmation"]   
 		end
+		private :check_confirmation_field_match
 
-	end
+	end # end ValidationsController
 
-end
+end # end Rapscallion module
